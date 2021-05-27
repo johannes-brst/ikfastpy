26cc61bfed36a3f41497a02d5de13e4bc1b49697
cpn <buerst@finf.uni-hannover.de>
2021-05-27 11:48:56 +0200

    problems with multible moving joints, pendulum

--- demo.py
+++ demo.py
@@ -8,21 +8,30 @@ import math
 
 def newJointSpeed(joint_config, actual_q, joint_speed, max_vel):
     print("newJointSpeed")
-    print(joint_config)
-    print(actual_q)
-    #TODO:"schnellste" Gelenk finden und als Faktor nutzen
-    for i in range(len(joint_config)):        
-        if(abs(joint_config[i] - actual_q[i]) > 0.01):
+    print("joint_config: "+str(joint_config))
+    print("actual_q: "+str(actual_q))
+    #TODO:"schnellste" Gelenk finden und als Faktor nutzen --> nur einzelnes Gelenk funktioniert bisher
+    for i in range(len(joint_config)):     
+        #if(i == 1):   
+        if(abs(joint_config[i] - actual_q[i]) > 0.001):
             if(joint_config[i] > 0 and actual_q[i] > 0):
                 if(joint_config[i] < actual_q[i]):
-                    joint_speed[i] = joint_config[i]*(-max_vel/joint_config[0])
+                    joint_speed[i] = joint_config[i]*(-max_vel/max(joint_config))
                 else:
-                    joint_speed[i] = math.copysign(joint_config[i]*max_vel/joint_config[0], (joint_config[i] - actual_q[i])+actual_q[i])
+                    joint_speed[i] = math.copysign(joint_config[i]*(max_vel/max(joint_config)), (joint_config[i] - actual_q[i])+actual_q[i])
+            elif(joint_config[i] < 0 and actual_q[i] < 0):
+                if(joint_config[i] > actual_q[i]):
+                    joint_speed[i] = joint_config[i]*(-max_vel/max(joint_config))
+                else:
+                    joint_speed[i] = math.copysign(joint_config[i]*(max_vel/max(joint_config)), (joint_config[i] - actual_q[i])+actual_q[i])
             else:
-                joint_speed[i] = math.copysign(joint_config[i]*max_vel/joint_config[0], (joint_config[i] - actual_q[i])+actual_q[i])
+                joint_speed[i] = math.copysign(joint_config[i]*(max_vel/max(joint_config)), (joint_config[i] - actual_q[i])+actual_q[i])
         else:
+            #if(abs(joint_config[i] - actual_q[i]) > 0.005):
+            #    joint_speed[i] = joint_speed[i] * -1
+            #else:
             joint_speed[i] = 0
-    print(joint_speed)
+    print("joint_speed: "+str(joint_speed))
     return joint_speed
 
 def findClosestSolution(joint_configs, actual_q):
@@ -66,11 +75,11 @@ print("\nTest passed!")
 
 rtde_c = rtde_control.RTDEControlInterface("127.0.0.1")
 rtde_r = rtde_receive.RTDEReceiveInterface("127.0.0.1")
-rtde_c.moveJ([1.755020, -1.131027, 2.141150,  0.456918,  0.842970, -0.346717])
+rtde_c.moveJ([0.555020, -1.131027, 2.141150,  0.456918,  0.842970, -0.346717])
 actual_q = rtde_r.getActualQ()
 print("actual_q: " + str(actual_q))
 joint_speed = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-dt = 0.002 #1.0/500 
+dt = 0.02 #1.0/500 
 max_vel = 3.14
 acceleration = 0.5
 joint_config = joint_configs[findClosestSolution(joint_configs,actual_q)]
