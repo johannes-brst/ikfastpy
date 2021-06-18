986979b8d147984b8f24c1ca5d1c86e3f0ee06ef
cpn <buerst@finf.uni-hannover.de>
2021-06-18 12:59:46 +0200

    added rodrigues in cpp, matrix/vector operations faulty

--- cpp-version/calculate_kinematics.cpp
+++ cpp-version/calculate_kinematics.cpp
@@ -5,6 +5,8 @@
 #include <thread>
 #include <chrono>
 #include <vector>
+#include <cmath>
+#include <numeric>
 #include "ikfast_wrapper.h"
 using namespace ur_rtde;
 using namespace std::chrono;
@@ -39,23 +41,41 @@ std::string vec2csv(const std::vector<T>& vec) {
 }
 
 template<typename T>
-std::vector<float> vecSubtract(const std::vector<T>& vec1, const std::vector<T>& vec2) {
-    std::vector<float> result = {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f};
+std::vector<double> vecSubtract(const std::vector<T>& vec1, const std::vector<T>& vec2) {
+    std::vector<double> result(vec1.size(),0.0);
   for (int i = 0; i <  vec1.size(); ++i) {
-      result[i] = vec1[i] - vec2[i];
+      result.at(i) = vec1.at(i) - vec2.at(i);
   }
   return result;
+}
+
+template<typename T>
+std::vector<double> vecAdd(const std::vector<T>& vec1, const std::vector<T>& vec2) {
+    std::vector<double> result(vec1.size(),0.0);
+  for (int i = 0; i <  vec1.size(); ++i) {
+      result.at(i) = vec1.at(i) + vec2.at(i);
+  }
+  return result;
+}
+
+template<typename T>
+std::vector<double> vecAddValue(const std::vector<T>& vec1, const T value) {
+    std::vector<double> result(vec1.size(),0.0);
+    for (int i = 0; i <  vec1.size(); ++i) {
+        result.at(i) = vec1.at(i) + value;
+    }
+    return result;
 }
 
 int findClosestSolution(std::vector<double> actual_q)
 {
     printf("findClosestSolution\n");
     int index_min = 0;
-    float min = 999.0f;
-    float difference = 0.0f;
+    double min = 999.0;
+    double difference = 0.0;
     for(int i = 0; i < 8; i++){
         if(sizeof(solutions[i]) > 1){
-            difference = 0.0f;
+            difference = 0.0;
             for(int j = 0; j < actual_q.size()-1; j++){
                 difference += std::abs(solutions[i][j] - actual_q[j]);
             }
@@ -70,18 +90,18 @@ int findClosestSolution(std::vector<double> actual_q)
     return index_min;
 }
 
-std::vector<double> newJointSpeed(std::vector<double> joint_config, std::vector<double> actual_q, std::vector<double> joint_speed, float max_vel)
+std::vector<double> newJointSpeed(std::vector<double> joint_config, std::vector<double> actual_q, std::vector<double> joint_speed, double max_vel)
 {
     //printf("newJointSpeed\n");
-    std::vector<float> tmp_speed = vecSubtract(joint_config, actual_q);
-    std::vector<float> abs_tmp_speed= {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f};
+    std::vector<double> tmp_speed = vecSubtract(joint_config, actual_q);
+    std::vector<double> abs_tmp_speed= {0.0,0.0,0.0,0.0,0.0,0.0};
     for(int i = 0; i < tmp_speed.size() - 1; i++){
         abs_tmp_speed[i] = std::abs(tmp_speed[i]);
     }
     for(int i = 0; i < joint_config.size() ; i++){
-        if(std::abs(tmp_speed[i]) > 0.002f){
+        if(std::abs(tmp_speed[i]) > 0.002){
             joint_speed[i] = std::min(tmp_speed[i]*5,max_vel);
-            if(*max_element(abs_tmp_speed.begin(),abs_tmp_speed.end()) > 3.14f){
+            if(*max_element(abs_tmp_speed.begin(),abs_tmp_speed.end()) > 3.14){
                 joint_speed[i] = tmp_speed[i]*(max_vel/ *max_element(abs_tmp_speed.begin(),abs_tmp_speed.end()));
             }
         }
@@ -166,7 +186,7 @@ void moveArm(std::string ip, std::vector<float> ee_pose){
     calculateKinematics(ee_pose);
     printf("\n");
 
-    float sT = 0.002; //sample Time
+    double sT = 0.002; //sample Time
     RTDEControlInterface rtde_control(ip);
     RTDEReceiveInterface rtde_receive(ip);
     //std::vector<double> startpos = {1.755020, -1.131027, 2.141150,  0.456918,  0.842970, -0.346717};
@@ -178,9 +198,9 @@ void moveArm(std::string ip, std::vector<float> ee_pose){
     
     std::vector<double> goal = solutions[findClosestSolution(actual_q)];
     
-    float max_vel = 3.14f;
-    float acceleration = 40.0f;
-    float dt = 0.02f;
+    double max_vel = 3.14;
+    double acceleration = 40.0;
+    double dt = 0.02;
     actual_q = rtde_receive.getActualQ();
 
     printf("current joint angles:\n");
@@ -191,7 +211,7 @@ void moveArm(std::string ip, std::vector<float> ee_pose){
 
     bool is_all_zero = true;
     for(int i = 0; i < joint_speed.size(); i++){
-        if(joint_speed[i] != 0.0f){
+        if(joint_speed[i] != 0.0){
             is_all_zero = false;
             break;
         }
@@ -215,7 +235,7 @@ void moveArm(std::string ip, std::vector<float> ee_pose){
 
         is_all_zero = true;
         for(int i = 0; i < joint_speed.size(); i++){
-            if(joint_speed[i] != 0.0f){
+            if(joint_speed[i] != 0.0){
                 is_all_zero = false;
                 break;
             }
@@ -236,11 +256,88 @@ void moveArm(std::string ip, std::vector<float> ee_pose){
 }
 
 
+template<typename T>
+std::vector<double>  DivideVectorByValue(const std::vector<T>& v, const T k){
+    std::vector<double> result(v.size(), 0.0);// = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    for (int i = 0; i < v.size(); ++i) {
+        result.at(i) = v.at(i) / k;
+    }
+
+    return result;
+}
+
+template<typename T>
+std::vector<double>  MultiplyVectorByValue(const std::vector<T>& v, const T k){
+    std::vector<double> result(v.size(), 0.0);// = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    for (int i = 0; i < v.size(); ++i) {
+        result.at(i) = v.at(i) * k;
+    }
+
+    return result;
+}
+
+template<typename T>
+std::vector<double>  MultiplyVector(const std::vector<T>& v, const std::vector<T>& k){
+    std::vector<double> result(v.size(), 0.0);// = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    for (int i = 0; i < v.size(); ++i) {
+        result.at(i) = v.at(i) * k.at(i);
+    }
+
+    return result;
+}
+   
+template<typename T>
+std::vector<double> S(const std::vector<T>& n){
+    std::vector<double> Sn(9, 0.0);// = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    Sn.at(1) = n.at(2) * -1;
+    Sn.at(2) = n.at(1);
+    Sn.at(3) = n.at(2);
+    Sn.at(5) = n.at(0) * -1;
+    Sn.at(6) = n.at(1) * -1;
+    Sn.at(7) = n.at(0);
+    return Sn;
+}
+
+/*Rodrigues formula
+Input: 1x3 array of rotations about x, y, and z
+Output: 3x3 rotation matrix*/
+std::vector<double> rodrigues(std::vector<double>& r){
+    double norm = 0.0;
+    for (int i = 0; i < r.size(); ++i) {
+        norm += r.at(i) * r.at(i);
+    }
+    double theta = norm;
+    std::vector<double> R(9, 0.0);// = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    if(theta > 1e-30){
+        std::vector<double> n = DivideVectorByValue(r, theta);
+        std::vector<double> Sn = S(n);
+        std::vector<double> eye(9, 0.0);// = {1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0};
+        eye.at(0) = 1.0;
+        eye.at(4) = 1.0;
+        eye.at(8) = 1.0;
+        std::vector<double> mult = MultiplyVectorByValue(Sn, sin(theta));
+        R = vecAdd(eye, mult);
+        R = vecAddValue(R, ((1 - std::cos(theta)) * std::inner_product(std::begin(Sn), std::end(Sn), std::begin(Sn), 0.0)));
+    }
+    else{
+        std::vector<double> Sr = S(r);
+        double theta2 = theta*theta;
+        std::vector<double> eye(9, 0.0);// = {1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0};
+        eye.at(0) = 1.0;
+        eye.at(4) = 1.0;
+        eye.at(8) = 1.0;
+        R = vecAdd(eye, MultiplyVectorByValue(Sr, 1 - sin(theta2)/6.0));
+        R = vecAddValue(R, (0.5f - std::cos(theta2)/24.0f) * std::inner_product(std::begin(Sr), std::end(Sr), std::begin(Sr), 0.0));
+    }
+    
+    return R;
+}
+
 int main(int argc, char *argv[])
 {
 
     // TODO: rodrigues calculation in cpp for rotation vector -> rotation matrix
-    //std::vector<float> ee_pose = { 0.99948378f, 0.01387675f, -0.02897591f, -0.000905f,
+    //std::vector<double> ee_pose = { 0.99948378f, 0.01387675f, -0.02897591f, -0.000905f,
     //-0.02259421f ,-0.33758036f ,-0.94102551f  ,-0.330611f,
     //-0.02284007f,  0.94119442f ,-0.33709256f  ,0.375722f};
 
@@ -254,5 +351,16 @@ int main(int argc, char *argv[])
     std::vector<float> ee_pose = ur5e_kin.forward(joint_angles);
 
     moveArm("127.0.0.1", ee_pose);
+
+    printf("Starting rodrigues test:\n");
+    std::vector<double> test = {1.914787, -0.006242, -0.037102};
+    std::vector<double> rod_test(9);
+    rod_test = rodrigues(test);
+    printf("Test: \n");
+    for(int j = 0; j < rod_test.size() - 1; j++){
+        std::cout << rod_test.at(j);        
+        printf("\n");
+    }
+
     return 0;
 }
